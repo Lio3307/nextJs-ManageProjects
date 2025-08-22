b1a7d3b9f1d550c674a30d0a6b042ba9eec31a58
Lio3307 <aureliofs01@gmail.com>
2025-08-22 19:39:38 +0800

    adding logout

---
+++ src/components/logout.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { authClient } from "@/lib/auth-client"
+import { Button } from "./ui/button";
+import { LogOutIcon } from "lucide-react";
+
+export default function Logout(){
+
+    const handleLogOut = async () => {
+        await authClient.signOut();
+    }
+    return  (
+        <Button variant="outline" onClick={handleLogOut}>Log out <LogOutIcon className="size-4"/> </Button>
+    )
+}
