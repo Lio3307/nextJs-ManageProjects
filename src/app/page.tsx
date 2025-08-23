c69ad14f5c96efc0bb4290a0767f739be564d659
Lio3307 <aureliofs01@gmail.com>
2025-08-23 13:34:42 +0800

    adding logout button to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+import LogedOut from "@/components/logout";
 import { buttonVariants } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
   return (
     <>
-      <p>welcome</p>
-      <Link href="/add-project" className={buttonVariants()}>
-        Add Project
-      </Link>
+      {session ? (
+        <div className="flex ">
+          <p>welcome</p>
+          <Link href="/add-project" className={buttonVariants()}>
+            Add Project
+          </Link>
+          <LogedOut/>
+        </div>
+      ) : (
+        <>
+          <p>welcome</p>
+          <Link href="/add-project" className={buttonVariants()}>
+            Add Project
+          </Link>
+        </>
+      )}
     </>
   );
 }
--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,15 +1,16 @@
-"use client"
+"use client";
 
-import { authClient } from "@/lib/auth-client"
+import { authClient } from "@/lib/auth-client";
 import { Button } from "./ui/button";
 import { LogOutIcon } from "lucide-react";
 
-export default function Logout(){
-
-    const handleLogOut = async () => {
-        await authClient.signOut();
-    }
-    return  (
-        <Button variant="outline" onClick={handleLogOut}>Log out <LogOutIcon className="size-4"/> </Button>
-    )
+export default function LogedOut() {
+  const handleLogOut = async () => {
+    await authClient.signOut();
+  };
+  return (
+    <Button variant="outline" onClick={handleLogOut}>
+      Log out <LogOutIcon className="size-4" />{" "}
+    </Button>
+  );
 }
