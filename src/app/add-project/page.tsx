151d5a5f431e25f93ef370e7f7bf4c364cd3bb51
Lio3307 <aureliofs01@gmail.com>
2025-08-23 15:11:42 +0800

    adding tiptap

--- src/app/add-project/page.tsx
+++ src/app/add-project/page.tsx
@@ -1,4 +1,7 @@
+import Tiptap from "@/components/tiptap-editor";
 import { Card, CardDescription, CardHeader, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function AddProject(){
     return (
@@ -8,7 +11,13 @@ export default function AddProject(){
                         <CardHeader>Create Project Manager</CardHeader>
                         <CardDescription>Create your own project note</CardDescription>
                         <CardContent>
-                            <form className=""></form>
+                            <form className="flex flex-col gap-4">
+                            <div className="flex flex-col gap-2">
+                                <Label>Title</Label>
+                                <Input name="title" required type="text"/>
+                            </div>
+                            <Tiptap/>
+                            </form>
                         </CardContent>
                     </Card>
             </div>
