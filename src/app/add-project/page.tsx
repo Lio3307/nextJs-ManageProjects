6bc27241c62e870b490b6c7a744d9cbabb2348c7
Lio3307 <aureliofs01@gmail.com>
2025-08-23 14:41:45 +0800

    install tiptap rich editor

--- src/app/add-project/page.tsx
+++ src/app/add-project/page.tsx
@@ -1,5 +1,17 @@
+import { Card, CardDescription, CardHeader, CardContent } from "@/components/ui/card";
+
 export default function AddProject(){
     return (
-        <p>Add project</p>
+        <>
+            <div className="flex justify-center">
+                    <Card>
+                        <CardHeader>Create Project Manager</CardHeader>
+                        <CardDescription>Create your own project note</CardDescription>
+                        <CardContent>
+                            <form className=""></form>
+                        </CardContent>
+                    </Card>
+            </div>
+        </>
     )
 }
