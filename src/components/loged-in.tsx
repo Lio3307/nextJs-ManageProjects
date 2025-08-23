2c4030588e2577c39e5d23705fcca4bc2216d790
Lio3307 <aureliofs01@gmail.com>
2025-08-23 13:11:28 +0800

    fix: user can login and signup again after already registereed/logged in

---
+++ src/components/loged-in.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { Card, CardContent, CardTitle } from "./ui/card";
+import { buttonVariants } from "./ui/button";
+
+export default function LogedIn() {
+  return (
+    <>
+      <div className="flex justify-center ">
+        <Card>
+          <CardContent>
+            <CardTitle className="mb-4">You already registered</CardTitle>
+            <Link className={buttonVariants()} href={"/"}>Back Home</Link>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}
