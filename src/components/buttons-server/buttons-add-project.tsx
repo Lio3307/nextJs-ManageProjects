1f7b36aa5674835085e9378b06e4fc2a06ec4789
Lio3307 <aureliofs01@gmail.com>
2025-08-24 11:37:40 +0800

    adding prisma Accelerate

--- src/components/buttons-server/buttons-add-project.tsx
+++ src/components/buttons-server/buttons-add-project.tsx
@@ -1,10 +1,10 @@
-import { PrismaClient } from "@prisma/client";
 import { Button } from "../ui/button";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 import { toast } from "sonner";
+import prisma from "@/lib/prisma";
 
 export default async function ButtonAddProjects({
   content,
@@ -13,7 +13,7 @@ export default async function ButtonAddProjects({
   content: string;
   title: string;
 }) {
-  const prisma = new PrismaClient();
+
 
   const session = await auth.api.getSession({
     headers: await headers(),
