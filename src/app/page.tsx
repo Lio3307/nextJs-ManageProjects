4bb94436b0a16dacb68b406885a09bd31618d3e8
Lio3307 <aureliofs01@gmail.com>
2025-08-25 16:58:41 +0800

    change home page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import LogedOut from "@/components/logout";
+import ProjectCard from "@/components/project-task/project-card";
 import { buttonVariants } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { ArrowRight, Plus } from "lucide-react";
 import { headers } from "next/headers";
 import Link from "next/link";
 
@@ -9,18 +11,77 @@ export default async function Home() {
     headers: await headers(),
   });
 
+  const data = await prisma.project.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 4,
+  });
+
+  if (!session) {
+    return (
+      <>
+        <div className="p-6">
+          <h1 className="text-2xl font-semibold">Welcome</h1>
+          <div className="flex items-center justify-between">
+            <p>You not registered yet, want to create account?</p>
+            <Link className={buttonVariants()} href="/login">
+              sign In
+            </Link>
+            <Link
+              className={buttonVariants({ variant: "secondary" })}
+              href="/signup"
+            >
+              sign Up
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen space-y-6 bg-gray-50">
-      <h1 className="text-2xl font-semibold">Welcome </h1>
+    <>
+      {data.length === 0 ? (
+        <div className="p-6">
+          <h1 className="text-2xl font-semibold">
+            Welcome <span className="font-none">{session.user.name}</span>
+          </h1>
+          <div className="flex items-center justify-between">
+            <p>You dont have a project yet</p>
+            <Link href="/add-project">
+              <Plus /> Create One
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div className="p-6 w-full">
+          <h1 className="text-2xl font-semibold">
+            Welcome <span className="font-none">{session.user.name}</span>
+          </h1>
+          <p className="text-xs text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, necessitatibus ex, porro, labore qui repellat placeat corporis ut amet hic eos voluptates veritatis! Sint voluptas ut adipisci dolores porro et.</p>
 
-      <div className="flex items-center gap-4">
-        <Link href="/add-project" className={buttonVariants()}>
-          Add Project
-        </Link>
+          <div className="">
 
-        {session && <LogedOut />}
-        {session && <Link href="/project-list">Project list</Link>}
-      </div>
-    </div>
+          
+          <div className="mt-6 flex justify-between">
+          <h1 className="text-[1.6rem] font-bold">Projects</h1>
+          <Link href="/add-project"><ArrowRight size="2.2rem"/></Link>
+        
+          </div>
+          <div className="h-[0.1rem] bg-black"></div>
+
+          <div className="flex flex-wrap mt-3 gap-4">
+            {data.map((item) => (
+              <ProjectCard key={item.id} data={item}/>
+            ))}
+          </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
