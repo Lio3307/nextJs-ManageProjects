fbefc0631be1ee7df2cd8922cf55b5395e2383ff
Lio3307 <aureliofs01@gmail.com>
2025-08-28 19:42:13 +0800

    fix: style when content json get rendered

--- src/components/json-content-parse/json-render.tsx
+++ src/components/json-content-parse/json-render.tsx
@@ -15,6 +15,7 @@ import Blockquote from "@tiptap/extension-blockquote";
 import { TextStyle } from "@tiptap/extension-text-style";
 import OrderedList from "@tiptap/extension-ordered-list";
 import BulletList from "@tiptap/extension-bullet-list";
+import clsx from "clsx";
 
 const lowlight = createLowlight(all);
 lowlight.register("html", html);
@@ -52,11 +53,19 @@ const JSONRender = ({ content }: { content: string }) => {
     content,
     editorProps: {
       attributes: {
-        class:
-          "prose prose-base sm:prose lg:prose-lg xl:prose-xl max-w-none " +
-          "focus:outline-none w-full min-h-[500px] p-6 " +
+        class: clsx(
+          "prose prose-base sm:prose lg:prose-lg xl:prose-xl max-w-none",
+          "focus:outline-none w-full min-h-[500px] p-6",
           "bg-background",
-
+          "[&_pre]:overflow-x-auto [&_pre]:max-w-full [&_pre]:p-4 [&_pre]:rounded-lg [&_pre]:!bg-gray-200 [&_pre]:!text-black",
+          "[&_code]:font-mono [&_code]:text-sm  [&_code]:!text-black",
+          "[&_li]:break-words [&_li]:pl-1",
+          "[&_ul]:pl-6 [&_ul]:list-disc [&_ol]:pl-6 [&_ol]:list-decimal",
+          "[&_input[type=checkbox]]:mr-2 [&_input[type=checkbox]]:align-mi  ddle",
+          "[&_table]:w-full [&_table]:max-w-full [&_table]:border-collapse",
+          "[&_td]:border [&_td]:px-2 [&_td]:py-1 [&_th]:border [&_th]:px-2 [&_th]:py-1",
+          "[&_img]:max-w-full [&_img]:h-auto"
+        ),
       },
     },
     immediatelyRender: false,
