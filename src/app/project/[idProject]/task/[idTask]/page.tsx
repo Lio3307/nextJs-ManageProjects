08882cfad2757f7dc6ebb188fd7e92a43faa0347
Lio3307 <aureliofs01@gmail.com>
2025-08-29 18:45:08 +0800

    adding Report model to schema

--- src/app/project/[idProject]/task/[idTask]/page.tsx
+++ src/app/project/[idProject]/task/[idTask]/page.tsx
@@ -1,45 +1,61 @@
 import { BreadcrumbWithCustomSeparator } from "@/components/breadcrumb-custom";
 import JSONRender from "@/components/json-content-parse/json-render";
-import prisma from "@/lib/prisma"
+import prisma from "@/lib/prisma";
 
-export default async function DetailTask({params}: {params: Promise<{idTask: string, idProject:string}>}){
+export default async function DetailTask({
+  params,
+}: {
+  params: Promise<{ idTask: string; idProject: string }>;
+}) {
+  const { idTask, idProject } = await params;
+  const projectName = await prisma.project.findUnique({
+    where: {
+      id: idProject,
+    },
+  });
 
-    const {idTask, idProject} = await params
-    const projectName = await prisma.project.findUnique({
-        where: {
-            id: idProject
-        }
-    })
+  if (!projectName) {
+    return;
+  }
 
-    if(!projectName){   
-            return;
-    }
+  const taskData = await prisma.task.findUnique({
+    where: {
+      id: idTask,
+    },
+  });
 
-    const taskData = await prisma.task.findUnique({
-        where: {
-            id: idTask
-        }
-    })
+  if (!taskData) {
+    throw new Error("Unknown Task");
+  }
 
-    if(!taskData){
-        throw new Error("Unknown Task");
-    }
-
-    return (
-        <>
-            <div className="p-4">
-                <BreadcrumbWithCustomSeparator name={projectName.title} link={`/project/${idProject}`}/>
-            <div className="flex justify-between mt-4">
-            <p className="text-xs txet-gray-600">Task by: <span className="text-xs txet-black font-bold">{taskData.createdBy}</span></p>
-            <p className="text-xs txet-gray-600">Created at: <span className="text-xs txet-black font-bold">{new Intl.DateTimeFormat("en-US", {
+  return (
+    <>
+      <div className="p-4">
+        <BreadcrumbWithCustomSeparator
+          name={projectName.title}
+          link={`/project/${idProject}`}
+        />
+        <div className="flex justify-between mt-4">
+          <p className="text-xs txet-gray-600">
+            Task by:{" "}
+            <span className="text-xs txet-black font-bold">
+              {taskData.createdBy}
+            </span>
+          </p>
+          <p className="text-xs txet-gray-600">
+            Created at:{" "}
+            <span className="text-xs txet-black font-bold">
+              {new Intl.DateTimeFormat("en-US", {
                 year: "numeric",
                 month: "long",
                 day: "numeric",
-              }).format(taskData.createdAt)}</span></p>
-            </div>
+              }).format(taskData.createdAt)}
+            </span>
+          </p>
+        </div>
 
-            <JSONRender content={JSON.parse(taskData.content)}/>
-            </div>
-        </>
-    )
+        <JSONRender content={JSON.parse(taskData.content)} />
+      </div>
+    </>
+  );
 }
