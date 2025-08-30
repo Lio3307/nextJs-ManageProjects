98e88de8a31e33554eb46662c0af79254570f768
Lio3307 <aureliofs01@gmail.com>
2025-08-30 21:47:40 +0800

    deletind detailed task(not component)

--- src/app/project/[idProject]/task/[idTask]/page.tsx
+++ src/app/project/[idProject]/task/[idTask]/page.tsx
@@ -42,26 +42,6 @@ export default async function DetailTask({
         taskData={taskData}
         reportData={reportData}
       />
-        <div className="flex justify-between mt-4">
-          <p className="text-xs txet-gray-600">
-            Task by:{" "}
-            <span className="text-xs txet-black font-bold">
-              {taskData.createdBy}
-            </span>
-          </p>
-          <p className="text-xs txet-gray-600">
-            Created at:{" "}
-            <span className="text-xs txet-black font-bold">
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              }).format(taskData.createdAt)}
-            </span>
-          </p>
-        </div>
-
-        <JSONRender content={JSON.parse(taskData.content)} />
       </div>
     </>
   );
