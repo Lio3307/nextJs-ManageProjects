4dfa1b50dd90150a699c8d7dbe03a9eb2aaade89
Lio3307 <aureliofs01@gmail.com>
2025-09-07 14:09:27 +0800

    adding new relation in schema

--- src/components/comment-component/comment-form.tsx
+++ src/components/comment-component/comment-form.tsx
@@ -12,46 +12,46 @@ export default async function CommentForm({ reportId }: { reportId: string }) {
   });
   return (
     <>
-    <div className="w-full">
- <div className="grid gap-4">
-   <div className="grid grid-cols-1">
-     <div className="flex justify-between">
-       <p className="font-bold text-sm">
-         Comment{" "}
-         <span className="text-xs text-gray-600 mx-2">
-           {commentReport.length > 0 ? commentReport.length : "0"}
-         </span>{" "}
-       </p>
-     </div>
-   </div>
-   
-   <div className="h-px bg-gray-300"></div>
-   
-   <div className="grid grid-cols-1">
-     <form action={handleComment}>
-       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-end">
-         <div className="">
-           <SubmitForm buttonName={<SendHorizonal/>} />
-         </div>
-       </div>
-       <div className="grid grid-cols-1 gap-4 mt-4">
-         <textarea
-         required
-           placeholder="Write comments..."
-           className="rounded-md text-sm p-3 border border-gray-300 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent resize-none"
-           name="comment"
-           rows={4}
-         />
-         <input name="reportId" type="hidden" value={reportId} />
-       </div>
-     </form>
-   </div>
-   
-   <div className="grid grid-cols-1">
-     <CommentCard dataComments={commentReport}/>
-   </div>
- </div>
- </div>
-</>
+      <div className="w-full">
+        <div className="grid gap-4">
+          <div className="grid grid-cols-1">
+            <div className="flex justify-between">
+              <p className="font-bold text-sm">
+                Comment{" "}
+                <span className="text-xs text-gray-600 mx-2">
+                  {commentReport.length > 0 ? commentReport.length : "0"}
+                </span>{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="h-px bg-gray-300"></div>
+
+          <div className="grid grid-cols-1">
+            <form action={handleComment}>
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-end">
+                <div className="">
+                  <SubmitForm buttonName={<SendHorizonal />} />
+                </div>
+              </div>
+              <div className="grid grid-cols-1 gap-4 mt-4">
+                <textarea
+                  required
+                  placeholder="Write comments..."
+                  className="rounded-md text-sm p-3 border border-gray-300 w-full placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent resize-none"
+                  name="comment"
+                  rows={4}
+                />
+                <input name="reportId" type="hidden" value={reportId} />
+              </div>
+            </form>
+          </div>
+
+          <div className="grid grid-cols-1">
+            <CommentCard dataComments={commentReport} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
