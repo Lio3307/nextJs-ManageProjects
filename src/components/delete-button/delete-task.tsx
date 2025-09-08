2646b0d513137a88109a19eb4394330b52fe3b00
Lio3307 <aureliofs01@gmail.com>
2025-09-08 21:37:04 +0800

    adding delete task button

---
+++ src/components/delete-button/delete-task.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { EllipsisVertical, Trash2 } from "lucide-react";
+
+export default function DeleteTask({ idTask }: { idTask: string }) {
+  const handleDelete = async () => {
+    const confirmDelete = confirm("Are you sure want to delete this Task?");
+    if (!confirmDelete) return;
+    await handleDeleteTask(idTask);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="p-2 rounded-md hover:bg-muted transition">
+          <EllipsisVertical className="h-5 w-5" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-40">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={handleDelete}
+          className="text-red-600 focus:text-red-600"
+        >
+          <Trash2 className="text-red-600 mr-2 h-4 w-4" />
+          Delete Task
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
