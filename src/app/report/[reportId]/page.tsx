f1db4a321f7e939cf9fb525dbfbaae406e72d17e
Lio3307 <aureliofs01@gmail.com>
2025-09-11 12:22:03 +0800

    showing button action only for specific user

--- src/app/report/[reportId]/page.tsx
+++ src/app/report/[reportId]/page.tsx
@@ -1,6 +1,10 @@
 import { BreadcrumbWithCustomSeparator } from "@/components/breadcrumb-custom";
 import CommentForm from "@/components/comment-component/comment-form";
 import prisma from "@/lib/prisma";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import DeleteReport from "@/components/delete-button/delete-report";
 
 export default async function ReportDetail({
   params,
@@ -8,6 +12,12 @@ export default async function ReportDetail({
   params: Promise<{ reportId: string }>;
 }) {
   const { reportId } = await params;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) return redirect("/login");
+
   const detailReport = await prisma.report.findUnique({
     where: {
       id: reportId,
@@ -19,18 +29,37 @@ export default async function ReportDetail({
   }
 
   const taskName = await prisma.task.findUnique({
-    where : {
-      id: detailReport.taskId
-    }
-  })
+    where: {
+      id: detailReport.taskId,
+    },
+  });
 
-  if(!taskName) throw new Error("Unknown Task");
-  
+  if (!taskName) throw new Error("Unknown Task");
+
+  const getProjectOwnerId = await prisma.project.findUnique({
+    where: {
+      id: taskName.projectId,
+    },
+  });
+
+  if (!getProjectOwnerId) throw new Error("Error get project");
 
   return (
     <>
       <div className="p-4">
-        <BreadcrumbWithCustomSeparator name={taskName.title} link={`/task/${detailReport.taskId}`} currentPageName="Report"/>
+        <div className="flex justify-between">
+          <BreadcrumbWithCustomSeparator
+            name={taskName.title}
+            link={`/task/${detailReport.taskId}`}
+            currentPageName="Report"
+          />
+          {session.user.id === detailReport.userId ||
+          session.user.id === getProjectOwnerId.userId ? (
+            <DeleteReport idReport={reportId} />
+          ) : (
+            ""
+          )}
+        </div>
         <p className="text-gray-600 my-2 text-xs">
           Posted at :{" "}
           <span className="text-black font-bold">
@@ -55,7 +84,7 @@ export default async function ReportDetail({
       </div>
 
       <div className="flex justify-center p-5 ">
-        <CommentForm reportId={reportId}/>
+        <CommentForm reportId={reportId} />
       </div>
     </>
   );
