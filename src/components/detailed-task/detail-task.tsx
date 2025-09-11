ce23474d0d6fdfea88dc59446fde8fa203dae02d
Lio3307 <aureliofs01@gmail.com>
2025-09-11 16:22:17 +0800

    showing task title

--- src/components/detailed-task/detail-task.tsx
+++ src/components/detailed-task/detail-task.tsx
@@ -6,26 +6,34 @@ import type { Task } from "@prisma/client";
 export default function DetailedTask({ detailedTask }: { detailedTask: Task }) {
   return (
     <>
-        <div className="flex justify-between mt-4">
-          <p className="text-xs txet-gray-600">
-            Task by:{" "}
-            <span className="text-xs txet-black font-bold">
-              {detailedTask.createdBy}
-            </span>
-          </p>
-          <p className="text-xs txet-gray-600">
-            Created at:{" "}
-            <span className="text-xs txet-black font-bold">
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              }).format(detailedTask.createdAt)}
-            </span>
-          </p>
-        </div>
 
-        <JSONRender content={JSON.parse(detailedTask.content)} />
+      <div className="flex justify-between mt-4">
+        <p className="text-xs txet-gray-600">
+          Task by:{" "}
+          <span className="text-xs txet-black font-bold">
+            {detailedTask.createdBy}
+          </span>
+        </p>
+        <p className="text-xs txet-gray-600">
+          Created at:{" "}
+          <span className="text-xs txet-black font-bold">
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            }).format(detailedTask.createdAt)}
+          </span>
+        </p>
+      </div>
+
+        <p className="p-2 text-xs my-2 txet-gray-600">
+          Task :{" "}
+          <span className="text-xs txet-black font-bold">
+            {detailedTask.title}
+          </span>
+        </p>
+
+      <JSONRender content={JSON.parse(detailedTask.content)} />
     </>
   );
 }
