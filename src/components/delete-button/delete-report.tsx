60d0121ac6cfcca78fbc65bb1290e4cec772b0fe
Lio3307 <aureliofs01@gmail.com>
2025-09-11 09:40:43 +0800

    adding action delete button for report

---
+++ src/components/delete-button/delete-report.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { handleDeleteReport } from "@/app/actions";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { EllipsisVertical, Trash2 } from "lucide-react";
+
+export default function DeleteReport({ idReport }: { idReport: string }) {
+  const handleDelete = async () => {
+    const confirmDelete = confirm("Are you sure want to delete this project?");
+    if (!confirmDelete) return;
+    await handleDeleteReport(idReport);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="p-2 rounded-md hover:bg-muted transition">
+          <EllipsisVertical className="h-5 w-5" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-40">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={handleDelete}
+          className="text-red-600 focus:text-red-600"
+        >
+          <Trash2 className="text-red-600 mr-2 h-4 w-4" />
+          Delete Report
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
