5c7190a5ea9e07d3f48a576e4a70bbcf316f9671
Lio3307 <aureliofs01@gmail.com>
2025-09-30 22:56:03 +0800

    update icon

--- src/components/report-list/report-list.tsx
+++ src/components/report-list/report-list.tsx
@@ -6,6 +6,7 @@ import type { Report } from "@prisma/client";
 import SubmitForm from "../submit-form";
 import { handleReport } from "@/app/actions";
 import Link from "next/link";
+import { MessageSquareWarning } from "lucide-react";
 
 export default function ReportList({
   reportData,
@@ -20,19 +21,7 @@ export default function ReportList({
         <div className="px-6 py-4 bg-gradient-to-r from-blue-50 to-indigo-50 border-b border-gray-200">
           <div className="flex items-center space-x-3">
             <div className="w-8 h-8 bg-blue-500 rounded-lg flex items-center justify-center">
-              <svg
-                className="w-4 h-4 text-white"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                />
-              </svg>
+              <MessageSquareWarning className="text-white h-[0.89rem]" />
             </div>
             <div>
               <h2 className="text-lg font-bold text-gray-900">
