344a36b500bdf1c0f6d5b7823f096005d8ab02b3
Lio3307 <aureliofs01@gmail.com>
2025-10-19 12:35:29 +0800

    adding navigation to request page

--- src/components/delete-button/delete-project.tsx
+++ src/components/delete-button/delete-project.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { EllipsisVertical, FilePen, Paperclip, Trash2 } from "lucide-react";
+import { Clock, EllipsisVertical, FilePen, Paperclip, Trash2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 export default function DeleteProject({ idProject, inviteCode }: { idProject: string, inviteCode: string }) {
@@ -53,12 +53,20 @@ export default function DeleteProject({ idProject, inviteCode }: { idProject: st
           <FilePen className="text-sky-600 mr-2 h-4 w-4" />
           Update Project
         </DropdownMenuItem>
+        <DropdownMenuSeparator />
         <DropdownMenuItem
           onClick={handleCopyInviteCode}
           className="text-sky-600 focus:text-sky-600"
         >
           <Paperclip className="text-gray-600 mr-1 h-2 w-2" />
           <span className="text-xs text-gray-600"> Copy invite code</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+        onClick={() => router.replace(`/project/${idProject}/request`)}
+          className="text-sky-600 focus:text-sky-600"
+        >
+          <Clock className="text-gray-600 mr-1 h-2 w-2" />
+          <span className="text-xs text-gray-600"> Check Request</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
