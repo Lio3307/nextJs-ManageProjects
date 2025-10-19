437ad1bd2559b90738aeb218d9dc0ab6556e379a
Lio3307 <aureliofs01@gmail.com>
2025-10-19 13:23:17 +0800

    fix: handle if member of project or someone else try to add task from copy paste the same url

--- src/app/project/[idProject]/add-task/page.tsx
+++ src/app/project/[idProject]/add-task/page.tsx
@@ -1,4 +1,8 @@
 import Tiptap from "@/components/tiptap-editor";
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { headers } from "next/headers";
+import { notFound, redirect } from "next/navigation";
 
 export default async function AddTask({
   params,
@@ -6,6 +10,20 @@ export default async function AddTask({
   params: Promise<{ idProject: string }>;
 }) {
   const { idProject } = await params;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) return redirect("/login");
+
+  const project = await prisma.project.findUnique({
+    where: {
+      id: idProject,
+    },
+  });
+
+  if (session.user.id !== project?.userId) return notFound();
 
   return (
     <>
