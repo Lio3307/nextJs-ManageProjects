40a576a0d64e04e628703b09f91cc9acac647d6f
Lio3307 <aureliofs01@gmail.com>
2025-11-02 14:50:47 +0800

    select account google when signin/signup with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,7 @@ const prisma = new PrismaClient();
 export const auth = betterAuth({
     socialProviders: {
         google: { 
+            prompt: "select_account", 
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
         }, 
